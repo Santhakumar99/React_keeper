40f74d81194c32b8775ba08db4486261d75c49b2
santhakumar99 <santhakumar9399@gmail.com>
2024-05-19 22:47:23 +0530

    sunday update list updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./Search";
 
 const Header = () => {
+
+  const [data ,SetData]= useState("")
+
+  const search = (e) => {
+    const value = e.target.value;
+    let inputValues = data;
+    inputValues = value;
+    SetData(inputValues);
+    
+    // onChange(text);        
+}
   return (
     <>
-   <div className="header">
-      <h2 className="header-text">Contacts</h2>
-      
-      </div>
+      <div className="header">
+       <h3 className="header-text">Contact List</h3>
+        </div>
     </>
   );
 };
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,43 +1,64 @@
-import {React, useState} from "react";
-import { FaSearch } from "react-icons/fa";
+import { React, useState, useEffect } from "react";
+// import { FaSearch } from "react-icons/fa";
 import ContactList from "./ContactList";
 import Avator from "./Avator";
+import Contacts from "./Contact";
+import Header from "./Header";
 
 const Search = () => {
-  const [products, setProducts] = useState(ContactList);
+  const [products, setProducts] = useState([]);
   const [searchVal, setSearchVal] = useState("");
-  function handleSearchClick(e) {
-    searchVal = e.target.value;
-    setSearchVal(searchVal);
-    if (searchVal === "") {
-      setProducts(ContactList);
-      return;
+
+  useEffect(() => {
+    setProducts(Contacts);
+  }, []);
+
+  const SearchOnChangeHandler = (e) => {
+    const value = e.target.value;
+    let inputValues = searchVal;
+    inputValues = value;
+    setSearchVal(inputValues);
+    if (value) {
+      const Projects = Contacts.filter(
+        (item) =>
+          (item.name != null &&
+            item.name.toLowerCase().includes(value.toLowerCase())) ||
+          (item.email != null &&
+            item.email.toLowerCase().includes(value.toLowerCase()))
+      );
+      console.log(Projects);
+      setProducts(Projects);
+    } else {
+      console.log("working");
+      setProducts(Contacts);
+      console.log(Contacts);
     }
-    const filterBySearch = ContactList.filter((item) => {
-      if (item.toLowerCase().includes(searchVal.toLowerCase())) {
-        return item;
-      }
-    });
-    setProducts(filterBySearch);
-  }
+  };
 
   return (
     <div>
-      <div className="search_section">
-        <input
-          className="search_box"
-          type="text"
-          value={searchVal}
-          onChange={(e)=>handleSearchClick()}
-           name="searchvalue"
-        />
+      <div className="headerSC">
+        <Header />
+        <div className="search_section">
+          <div className="container">
+            <input
+              type="text"
+              value={searchVal}
+              onChange={(e) => SearchOnChangeHandler(e)}
+              name="searchvalue"
+              class="form-control searchbox"
+              placeholder="Search"
+            />
+          </div>
+        </div>
       </div>
-      {products.map((item) => (
+      <div className="listdata">
+      {products!="" ?products.map((props) => (
         <div className="card_content">
           <div className="container">
             <div className="c_h_section">
               <div className="row">
-                <div className="col-2 c_d_section">
+                <div className="col-4 c_d_section">
                   <Avator img={props.img} />
                 </div>
                 <div className="col-8 name_section">
@@ -48,7 +69,8 @@ const Search = () => {
             </div>
           </div>
         </div>
-      ))}
+      )):<div className="NO Data">No Data Available !!!</div>}
+      </div>
     </div>
   );
 };
